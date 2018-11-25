afb22d71664a94e67f6c954b8a2c7ffdd23a7a57
Smirina <nitchenkoiv@gmail.com>
2018-11-25 22:44:24 +0200

    wip

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,22 +1,67 @@
 import 'package:flutter/material.dart';
 
-import './products.dart';
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
 
-class AuthPage extends StatelessWidget {
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue;
+  String _passwordValue;
+  bool _acceptTerms = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('LOGIN'),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
+        appBar: AppBar(
+          title: Text('Login'),
         ),
-      ),
-    );
+        body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: ListView(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'e-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (String value) {
+                  setState(() {
+                    _emailValue = value;
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'password'),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    _passwordValue = value;
+                  });
+                },
+              ),
+              SwitchListTile(
+                value: _acceptTerms,
+                onChanged: (bool value) {
+                  setState(() {
+                    _acceptTerms = value;
+                                    });
+                },
+                title: Text('Accept terms'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                child: Text('LOGIN'),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/');
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
